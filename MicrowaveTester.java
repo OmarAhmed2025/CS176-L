1a1bb064b0d5b0a6b9a85170807abe1875c805e6
OmarAhmed2025 <91150797+OmarAhmed2025@users.noreply.github.com>
2022-09-29 15:49:33 -0400

    MicrowaveTester

---
+++ MicrowaveTester.java
@@ -0,0 +1,14 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+	
+		Microwave myMicrowave = new Microwave();
+			myMicrowave.time();
+			myMicrowave.time();
+			myMicrowave.power();
+			myMicrowave.start();
+			myMicrowave.reset();
+	}
+
+}
